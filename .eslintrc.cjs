1bccde9a59329c4bc8240ec09fdc86cd1680864d
알파 <arpa1705@gmail.com>
2024-08-23 20:56:51 +0900

    fix: eslint - Prettier 충돌 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,12 +6,11 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-refresh', 'prettier'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
