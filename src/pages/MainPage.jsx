056e52b323a3f2832f6f3ae9e3e73d8c8b926a18
Giwon <jkw04257773@gmail.com>
2024-11-02 21:55:39 +0900

    fix: 좌우스크롤 제거

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,7 +5,7 @@ import '@/font/main_font.css';
 
 export default function MainPage() {
   return (
-    <>
+    <div style={{ overflowX: 'hidden' }}>
       <Section.Section1 />
       <Section.Section2 />
       <Section.Section3 />
@@ -13,6 +13,6 @@ export default function MainPage() {
       <Section.Section5 />
       <Section.Section6 />
       <Footer />
-    </>
+    </div>
   );
 }
