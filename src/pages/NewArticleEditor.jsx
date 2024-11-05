0666df28b88345ebf19e06e734fc3b5f3d53f40b
알파 <arpa1705@gmail.com>
2024-11-05 13:14:45 +0900

    feat: 게시글 등록 예외 처리

--- src/pages/NewArticleEditor.jsx
+++ src/pages/NewArticleEditor.jsx
@@ -154,10 +154,15 @@ export default function NewArticle() {
         tag: category || null,
         content: ref.current.getInstance().getMarkdown(),
       },
-    }).then((r) => {
-      alert('게시글이 성공적으로 등록되었습니다');
-      navigate(`/articles/${r.data.id}`);
-    });
+    })
+      .then((r) => {
+        alert('게시글이 성공적으로 등록되었습니다');
+        navigate(`/articles/${r.data.id}`);
+      })
+      .catch((e) => {
+        console.error(e);
+        alert('게시글을 등록하는 중 오류가 발생했습니다');
+      });
   };
 
   useEffect(() => {
