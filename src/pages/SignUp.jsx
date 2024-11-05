9f041f47467f84d0ee89628f9daa407c3f3fff54
Giwon <jkw04257773@gmail.com>
2024-11-06 00:09:17 +0900

    chore: 올바른 테마 적용 및 피그마 디자인과 동일한 레이아웃으로 수정

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,117 +5,128 @@ import styled from 'styled-components';
 import useAlert from '@/hooks/modal/useAlert';
 
 import { Text } from '@components/typograph/Text';
+import { Button } from '@components/forms/Button';
 import { Alert } from '@components/forms/modal/Alert';
 
 import { API } from '@/utils/api';
-import '@/font/main_font.css';
 
 const Container = styled.div`
-  background-color: #080f17;
-  display: flex;
-  justify-content: center;
   width: 100vw;
-  height: 100vh;
-  margin: 0;
-  padding: 0;
-`;
 
-const SignUpContainer = styled.div`
+  margin: 0;
+
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  flex-direction: row;
   justify-content: center;
-  height: 100%;
-  position: relative;
-  top: 120px;
-  padding-top: 60px;
-  padding-bottom: 150px;
+  align-items: center;
+
+  background-color: var(--body-background);
 `;
 
 const SignUpBox = styled.div`
-  background-color: #1b1e27;
+  width: 914px;
+
+  margin: 160px 0;
   padding: 50px;
-  border-radius: 10px;
-  width: 500px;
+  box-sizing: border-box;
+
+  display: flex;
+  flex-direction: column;
+  gap: 57px;
+
+  border-radius: 30px;
+
+  background-color: var(--container-primary-background);
+  border: 1px solid var(--container-border);
+
+  @media (max-width: 768px) {
+    margin: 100px 0;
+    padding: 30px;
+    border: none;
+  }
 `;
 
 const SignUpHeader = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: flex-start;
-  position: relative;
-  padding-bottom: 20px;
+  align-items: center;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `;
 
 const KertLogo = styled.div`
-  position: absolute;
-  top: 0;
-  right: 0;
+  width: 103px;
+  height: 101px;
 
-  img {
-    width: 80px;
-    height: 80px;
-    border-radius: 50%;
-  }
+  background-image: var(--square-logo-url);
+  background-size: cover;
+
+  opacity: 0.1;
 `;
 
 const SignUpForm = styled.form`
-  .input-group {
-    margin-bottom: 25px;
-    margin-right: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+`;
 
-    label {
-      display: block;
-      margin-bottom: 5px;
-      font-size: 18px;
-      color: #ffffff;
-    }
+const InputGroup = styled.div`
+  width: 100%;
 
-    input {
-      width: 100%;
-      padding: 15px;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-      background-color: #1c1f25;
-      color: white;
-      outline: none;
-    }
+  display: flex;
+  gap: 20px;
 
-    input:focus {
-      border-color: #3b82f6;
-      box-shadow: none;
-      background-color: #1c1f25; /* 배경색은 유지 */
-    }
-
-    input::placeholder {
-      font-size: 16px;
-    }
-
-    .error-message {
-      color: #ff4d4d;
-      font-size: 14px;
-      margin-top: 10px;
-    }
+  & > * {
+    width: 100%;
   }
 `;
 
-const SignUpButton = styled.button`
+const InputWrapper = styled.div`
+  & > span {
+    margin-left: 10px;
+    margin-bottom: 8px;
+  }
+
+  .error-message {
+    margin-left: 10px;
+    color: var(--danger-color);
+    font-size: 14px;
+    margin-top: 10px;
+  }
+`;
+
+const Input = styled.input`
+  transition:
+    border-color 0.2s ease-out,
+    background-color 0.2s ease-out;
+
   width: 100%;
-  padding: 15px;
-  background-color: #4a90e2;
-  border: none;
-  border-radius: 5px;
-  color: white;
-  font-size: 20px;
-  cursor: pointer;
-  margin-top: 20px;
+  height: 56px;
+
+  padding: 20px;
+  box-sizing: border-box;
+  border-radius: 14px;
+
+  background-color: var(--container-primary-background);
+  border: 1px solid var(--container-border);
+  color: var(--primary-text-color);
+  outline: none;
+
+  &:focus {
+    border: 1px solid #4a90e2; /* 파란색 테두리 */
+    box-shadow: none; /* 흰색 테두리 제거 */
+    background-color: var(--container-secondary-background);
+  }
 `;
 
 const LoginLink = styled.div`
-  margin-top: 20px;
   text-align: center;
   font-size: 14px;
-  color: #ccc;
+  color: var(--secondary-text-color);
 `;
 
 export default function SignUp() {
@@ -170,62 +181,69 @@ export default function SignUp() {
 
   return (
     <Container>
-      <SignUpContainer>
-        <SignUpBox>
-          <SignUpHeader>
-            <div>
-              <Text size="l" weight="bold" color="#ffffff">
-                Sign Up to KERT
-              </Text>
-              <Text size="sxl" weight="bold" color="#ffffff">
-                회원가입
-              </Text>
-            </div>
-            <KertLogo>
-              <img src="../logo/white_square.png" alt="kert-logo" />
-            </KertLogo>
-          </SignUpHeader>
-          <SignUpForm onSubmit={handleSubmit(onSubmit)}>
-            {/* name */}
-            <div className="input-group">
-              <label>이름</label>
-              <input
-                type="text"
-                placeholder="홍길동"
-                {...register('username', {
-                  required: '이름을 입력해주세요.',
-                  pattern: {
-                    value: /^[가-힣]{2,5}$/,
-                    message: '올바른 이름을 입력해주세요. (한글 2~5자)',
-                  },
-                })}
-              />
-              {errors.username && (
-                <p className="error-message">{errors.username.message}</p>
-              )}
-            </div>
-            {/* student number */}
-            <div className="input-group">
-              <label>학번</label>
-              <input
-                type="text"
-                placeholder="2024000000"
-                {...register('student', {
-                  required: '학번을 입력해주세요.',
-                  pattern: {
-                    value: /^[0-9]{10}$/,
-                    message: '학번은 숫자 10자리로 입력해주세요.',
-                  },
-                })}
-              />
-              {errors.student && (
-                <p className="error-message">{errors.student.message}</p>
-              )}
-            </div>
+      <SignUpBox>
+        <SignUpHeader>
+          <TitleWrapper>
+            <Text size="m" weight="light" color="--secondary-text-color">
+              Sign Up to KERT
+            </Text>
+            <Text size="sxl" weight="bold">
+              회원가입
+            </Text>
+          </TitleWrapper>
+          <KertLogo />
+        </SignUpHeader>
+        <SignUpForm onSubmit={handleSubmit(onSubmit)}>
+          <div
+            style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}
+          >
+            {/* name & student_id */}
+            <InputGroup>
+              <InputWrapper>
+                <Text size="s" weight="regular">
+                  이름
+                </Text>
+                <Input
+                  type="text"
+                  placeholder="홍길동"
+                  {...register('username', {
+                    required: '이름을 입력해주세요.',
+                    pattern: {
+                      value: /^[가-힣]{2,5}$/,
+                      message: '올바른 이름을 입력해주세요. (한글 2~5자)',
+                    },
+                  })}
+                />
+                {errors.username && (
+                  <p className="error-message">{errors.username.message}</p>
+                )}
+              </InputWrapper>
+              <InputWrapper>
+                <Text size="s" weight="regular">
+                  학번
+                </Text>
+                <Input
+                  type="text"
+                  placeholder="2024000000"
+                  {...register('student', {
+                    required: '학번을 입력해주세요.',
+                    pattern: {
+                      value: /^[0-9]{10}$/,
+                      message: '학번은 숫자 10자리로 입력해주세요.',
+                    },
+                  })}
+                />
+                {errors.student && (
+                  <p className="error-message">{errors.student.message}</p>
+                )}
+              </InputWrapper>
+            </InputGroup>
             {/* e-mail */}
-            <div className="input-group">
-              <label>이메일</label>
-              <input
+            <InputWrapper>
+              <Text size="s" weight="regular">
+                이메일
+              </Text>
+              <Input
                 type="email"
                 placeholder="kert@gmail.com"
                 {...register('mail', {
@@ -239,78 +257,86 @@ export default function SignUp() {
               {errors.mail && (
                 <p className="error-message">{errors.mail.message}</p>
               )}
-            </div>
-            {/* major */}
-            <div className="input-group">
-              <label>전공</label>
-              <input
-                type="text"
-                placeholder="심컴/글솝/플솝"
-                {...register('major', {
-                  required: '전공을 입력해주세요.',
-                  pattern: {
-                    value: /^[가-힣]{2}$/,
-                    message: '올바른 형식으로 입력해주세요. ',
-                  },
-                })}
-              />
-              {errors.major && (
-                <p className="error-message">{errors.major.message}</p>
-              )}
-            </div>
-            {/* generation */}
-            <div className="input-group">
-              <label>기수</label>
-              <input
-                type="text"
-                placeholder="2024-1"
-                {...register('generation', {
-                  required: '기수를 입력해주세요.',
-                  pattern: {
-                    value: /^(20\d{2})-(1|2)$/,
-                    message:
-                      '기수는 "연도-학기" 형식으로 입력해주세요. 예: 2024-1',
-                  },
-                })}
-              />
-              {errors.generation && (
-                <p className="error-message">{errors.generation.message}</p>
-              )}
-            </div>
-            {/* password */}
-            <div className="input-group">
-              <label>비밀번호</label>
-              <input
-                type="password"
-                placeholder="비밀번호"
-                {...register('password', {
-                  required: '비밀번호를 입력해주세요.',
-                  minLength: {
-                    value: 8,
-                    message: '비밀번호는 8자리 이상이여야 합니다. ',
-                  },
-                  pattern: {
-                    value:
-                      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,20}$/,
-                    message:
-                      '비밀번호는 숫자, 영문 대문자·소문자, 특수문자를 포함해야 합니다.',
-                  },
-                })}
-              />
-              {errors.password && (
-                <p className="error-message">{errors.password.message}</p>
-              )}
-            </div>
-
-            <SignUpButton type="submit">회원가입</SignUpButton>
-            <LoginLink>
-              이미 계정이 있으신가요? <Link to="/login">로그인</Link>
-            </LoginLink>
-          </SignUpForm>
-        </SignUpBox>
-        {/* Alert 컴포넌트 렌더링 */}
-        <Alert isOpen={isOpen} closeAlert={closeAlert} />
-      </SignUpContainer>
+            </InputWrapper>
+            {/* major & generation */}
+            <InputGroup>
+              <InputWrapper>
+                <Text size="s" weight="regular">
+                  전공
+                </Text>
+                <Input
+                  type="text"
+                  placeholder="심컴/글솝/플솝"
+                  {...register('major', {
+                    required: '전공을 입력해주세요.',
+                    pattern: {
+                      value: /^[가-힣]{2}$/,
+                      message: '올바른 형식으로 입력해주세요. ',
+                    },
+                  })}
+                />
+                {errors.major && (
+                  <p className="error-message">{errors.major.message}</p>
+                )}
+              </InputWrapper>
+              <InputWrapper>
+                <Text size="s" weight="regular">
+                  기수
+                </Text>
+                <Input
+                  type="text"
+                  placeholder="2024-1"
+                  {...register('generation', {
+                    required: '기수를 입력해주세요.',
+                    pattern: {
+                      value: /^(20\d{2})-(1|2)$/,
+                      message:
+                        '기수는 "연도-학기" 형식으로 입력해주세요. 예: 2024-1',
+                    },
+                  })}
+                />
+                {errors.generation && (
+                  <p className="error-message">{errors.generation.message}</p>
+                )}
+              </InputWrapper>
+            </InputGroup>
+          </div>
+          {/* password */}
+          <InputWrapper>
+            <Text size="s" weight="regular">
+              비밀번호
+            </Text>
+            <Input
+              type="password"
+              placeholder="비밀번호"
+              {...register('password', {
+                required: '비밀번호를 입력해주세요.',
+                minLength: {
+                  value: 8,
+                  message: '비밀번호는 8자리 이상이여야 합니다. ',
+                },
+                pattern: {
+                  value:
+                    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,20}$/,
+                  message:
+                    '비밀번호는 숫자, 영문 대문자·소문자, 특수문자를 포함해야 합니다.',
+                },
+              })}
+            />
+            {errors.password && (
+              <p className="error-message">{errors.password.message}</p>
+            )}
+          </InputWrapper>
+          <Button width="100%" height="60px">
+            회원가입
+          </Button>
+          <LoginLink>
+            이미 계정이 있으신가요? <Link to="/login">로그인</Link>
+          </LoginLink>
+        </SignUpForm>
+      </SignUpBox>
+      {/* Alert 컴포넌트 렌더링 */}
+      <Alert isOpen={isOpen} closeAlert={closeAlert} />
     </Container>
   );
 }
